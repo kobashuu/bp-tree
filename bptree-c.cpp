f8723f63545a2189f40220a8de6e6acee0c70de3
小林秀太 <steven@kobayashishuutanoMacBook-Pro.local>
2021-04-18 18:01:59 +0900

    change a strcture of class

--- bptree-c.cpp
+++ bptree-c.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 class BPT {
+   private:
     class Temp {
        public:
         int key[CHILD_NUMBER];
@@ -10,73 +11,71 @@ class BPT {
 
     Temp *temp = new Temp;
 
-    void split(Temp *temp) {
-        this->key[0] = temp->key[0];
-        this->key[1] = temp->key[1];
-        Node *node = new Node;
-        node->key[0] = temp->key[2];
-        node->key[1] = temp->key[3];
-    }
-
-    void make_rootnode() { Node *node = new Node; }
-
-    void insert(int key) {
-        if (this->nkey < CHILD_NUMBER) {  // nodeがまだいっぱいじゃない場合
-            insert_and_sort(key);
-        } else {
-            copy_to_temp(key);
-            split(temp);
-        }
-    }
-
    public:
     class Node {
-       private:
-        void insert_and_sort(int key);
-        void copy_to_temp(int key);
-
        public:
         Node();
+        void insert_and_sort(int key);
+        void copy_to_temp(Temp *temp, int key);
         bool is_leaf;
         int nkey;
         int key[CHILD_NUMBER];
         Node *children[CHILD_NUMBER + 1];
     };
 
-    // Node初期化 //
-    Node::Node() : is_leaf(true), nkey(0) {}
-
-    void Node::insert_and_sort(int key) {
-        if (nkey == 0) {
-            this->key[0] = key;
-            this->nkey++;
-            return;
-        }
-        for (int i = this->nkey - 1; i >= 0; i--) {
-            if (this->key[i] <= key) {
-                this->key[i + 1] = key;
-                break;
-            } else {
-                this->key[i + 1] = this->key[i];
-                this->key[i] = key;
-            }
-        }
-        this->nkey++;
+    void split(Temp *temp) {
+        // this->key[0] = temp->key[0];
+        // this->key[1] = temp->key[1];
+        // Node *node = new Node;
+        // node->key[0] = temp->key[2];
+        // node->key[1] = temp->key[3];
     }
 
-    void Node::copy_to_temp(int key) {
-        cout << "copy to temp started" << endl;
-        for (int i = 0; i < CHILD_NUMBER; i++) {
-            temp->key[i] = this->key[i];  // tempにコピー
+    void make_rootnode() { Node *node = new Node; }
+
+    void insert(Node *node, int key) {
+        if (node->nkey < CHILD_NUMBER) {  // nodeがまだいっぱいじゃない場合
+            node->insert_and_sort(key);
+        } else {
+            node->copy_to_temp(temp, key);
+            split(temp);
         }
-        for (int i = CHILD_NUMBER - 1; i >= 0; i--) {
-            if (temp->key[i] <= key) {
-                temp->key[i + 1] = key;
-                break;
-            } else {
-                temp->key[i + 1] = temp->key[i];
-                temp->key[i] = key;
-            }
+    }
+};
+
+// Node初期化 //
+BPT::Node::Node() : is_leaf(true), nkey(0) {}
+
+void BPT::Node::insert_and_sort(int key) {
+    if (nkey == 0) {
+        this->key[0] = key;
+        this->nkey++;
+        return;
+    }
+    for (int i = this->nkey - 1; i >= 0; i--) {
+        if (this->key[i] <= key) {
+            this->key[i + 1] = key;
+            break;
+        } else {
+            this->key[i + 1] = this->key[i];
+            this->key[i] = key;
+        }
+    }
+    this->nkey++;
+}
+
+void BPT::Node::copy_to_temp(Temp *temp, int key) {
+    cout << "copy to temp started" << endl;
+    for (int i = 0; i < CHILD_NUMBER; i++) {
+        temp->key[i] = this->key[i];  // tempにコピー
+    }
+    for (int i = CHILD_NUMBER - 1; i >= 0; i--) {
+        if (temp->key[i] <= key) {
+            temp->key[i + 1] = key;
+            break;
+        } else {
+            temp->key[i + 1] = temp->key[i];
+            temp->key[i] = key;
         }
     }
 }
@@ -86,10 +85,6 @@ int main() {
     BPT::Node *node = new BPT::Node;
     int keys[4] = {3, 1, 2, 4};
     for (int i = 0; i < 4; i++) {
-        node->insert(keys[i]);
-    }
-    cout << "tempのなかみ確認" << endl;
-    for (int i = 0; i < 4; i++) {
-        cout << temp->key[i] << endl;
+        bpt->insert(node, keys[i]);
     }
 }
